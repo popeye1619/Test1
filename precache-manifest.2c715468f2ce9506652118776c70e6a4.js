f0e5b1337d9a033b67f04ded121d55fdded69c37
popeye1619 <abhowmik14@gmail.com>
2020-09-18 13:46:43 +0530

    Updates

---
+++ precache-manifest.2c715468f2ce9506652118776c70e6a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df369cb96cdbc4e3f94d04f9d13fe15",
+    "url": "/Test1/index.html"
+  },
+  {
+    "revision": "3d3cd05bf9df61b3420e",
+    "url": "/Test1/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "d38ff4312dd2c3892257",
+    "url": "/Test1/static/js/2.3b590a9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Test1/static/js/2.3b590a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3cd05bf9df61b3420e",
+    "url": "/Test1/static/js/main.deccdd37.chunk.js"
+  },
+  {
+    "revision": "f5231987c610081ba1d9",
+    "url": "/Test1/static/js/runtime-main.032fcef5.js"
+  },
+  {
+    "revision": "818eea624af7e35e3023198df432fb9b",
+    "url": "/Test1/static/media/caravan.818eea62.jpg"
+  },
+  {
+    "revision": "959041cd146bb204cfca6e6efc603658",
+    "url": "/Test1/static/media/faces.959041cd.jpg"
+  },
+  {
+    "revision": "870728150eb519274043d097afc71850",
+    "url": "/Test1/static/media/personalise.87072815.jpg"
+  },
+  {
+    "revision": "489f19ddd19f42b54f9af22c5fe4cb19",
+    "url": "/Test1/static/media/social.489f19dd.jpg"
+  },
+  {
+    "revision": "a6a7adc8edabd872c2692f5726cd52e7",
+    "url": "/Test1/static/media/t&l.a6a7adc8.jpg"
+  }
+]);
